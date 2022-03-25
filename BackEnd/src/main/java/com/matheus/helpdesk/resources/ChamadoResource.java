9a97037566588a23cbc5384937b5ec68e3217119
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-03-25 19:13:05 -0300

    Back-end update(Chamado)

--- BackEnd/src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
+++ BackEnd/src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,31 +27,34 @@ public class ChamadoResource {
 
 	@Autowired
 	private ChamadoService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
 		Chamado obj = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<ChamadoDTO>> findAll(){
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
-		Chamado obj = service.create(objDTO);
-		URI uri = ServletUriComponentsBuilder.
-				fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO obj) {
+		Chamado newObj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newObj.getId())
+				.toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	
-	
-	
-	
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+		Chamado newObj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+		
+	}
+
 }
