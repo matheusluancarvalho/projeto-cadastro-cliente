894ff8347671bde045af8ea6e56b215d290b77d2
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-03-25 18:41:51 -0300

    Primeiro Commit Back-end

--- BackEnd/src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
+++ BackEnd/src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
@@ -1,14 +1,20 @@
 package com.matheus.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheus.helpdesk.domain.Chamado;
 import com.matheus.helpdesk.domain.dtos.ChamadoDTO;
@@ -34,5 +40,17 @@ public class ChamadoResource {
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+		Chamado obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
+	
+	
 	
 }
--- BackEnd/src/main/java/com/matheus/helpdesk/services/ChamadoService.java
+++ BackEnd/src/main/java/com/matheus/helpdesk/services/ChamadoService.java
@@ -3,19 +3,30 @@ package com.matheus.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.helpdesk.domain.Chamado;
+import com.matheus.helpdesk.domain.Cliente;
+import com.matheus.helpdesk.domain.Tecnico;
+import com.matheus.helpdesk.domain.dtos.ChamadoDTO;
+import com.matheus.helpdesk.domain.enums.Prioridade;
+import com.matheus.helpdesk.domain.enums.Status;
 import com.matheus.helpdesk.repositories.ChamadoRepository;
 import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ChamadoService {
-	
+
 	@Autowired
 	private ChamadoRepository repository;
-	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
@@ -23,6 +34,31 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		
+		return chamado;
+		
 	}
 
 }
